9ce6f64c84ebb568435e076cd9085964be2461ba
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-01-11 00:48:29 +0100

    reformat next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const hostUrl = 'http://wwwlab.cs.univie.ac.at'; 
+const hostUrl = 'http://wwwlab.cs.univie.ac.at';
 const basePath = '/~gyarmatip41/app-store-clone-app';
 
 module.exports = {
